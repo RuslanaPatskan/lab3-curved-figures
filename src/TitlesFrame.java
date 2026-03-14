4c1856238eb5e6a9cebf55a6fc6672f765797f2b
Lana <152506042+RuslanaPatskan@users.noreply.github.com>
2026-03-14 14:05:46 +0100

    Initial import: decompiled sources from lab3.jar

---
+++ src/TitlesFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class TitlesFrame extends JFrame {
+
+    public TitlesFrame() {
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Кривые фигуры");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new TitlesPanel(78));
+        setSize(350, 350);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                TitlesFrame frame = new TitlesFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
